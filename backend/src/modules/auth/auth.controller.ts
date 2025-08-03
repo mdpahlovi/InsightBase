a906797296d312a1b0aaefdd6e1ef107c7efca85
MD Pahlovi <mdpahlovi07@gmail.com>
2025-08-03 16:59:53 +0600

    cookies set from frontend

--- backend/src/modules/auth/auth.controller.ts
+++ backend/src/modules/auth/auth.controller.ts
@@ -6,32 +6,20 @@ export class AuthController {
     private authService = new AuthService();
 
     signup = catchAsync(async (req: Request, res: Response) => {
-        const { user, token } = await this.authService.signup(req.body);
-
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: false,
-            sameSite: "lax",
-        });
+        const data = await this.authService.signup(req.body);
 
         res.status(201).json({
             message: "Signup successfully",
-            data: { user },
+            data,
         });
     });
 
     signin = catchAsync(async (req: Request, res: Response) => {
-        const { user, token } = await this.authService.signin(req.body);
-
-        res.cookie("token", token, {
-            httpOnly: true,
-            secure: false,
-            sameSite: "lax",
-        });
+        const data = await this.authService.signin(req.body);
 
         res.status(200).json({
             message: "Signin successfully",
-            data: { user },
+            data,
         });
     });
 
--- frontend/src/lib/axios.ts
+++ frontend/src/lib/axios.ts
@@ -4,7 +4,13 @@ const BASE_URL = process.env.NEXT_PUBLIC_SERVER_URL;
 const axios = baseAxios.create({ baseURL: `${BASE_URL}/api/v1`, timeout: 60000, withCredentials: true });
 
 axios.interceptors.response.use(
-    (response) => response,
+    (response) => {
+        if (response?.data?.data?.token) {
+            document.cookie = `token=${response.data.data.token}; path=/; expires=${new Date(Date.now() + 60 * 60 * 24 * 7).toUTCString()}`;
+        }
+
+        return response;
+    },
     (error) => {
         return Promise.reject({
             status: error?.response?.status || 500,
